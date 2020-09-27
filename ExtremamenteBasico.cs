930b3bc5a57d3489314644fafc483c22b7c241e3
Douglas Soares de Souza <douglassouzabjj@hotmail.com>
2020-09-27 16:45:07 -0300

    Extremamento Basico completo

---
+++ ExtremamenteBasico.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ExtremamentoBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            x = a + b;
+
+            Console.WriteLine("X = " + x);
+        }
+    }
+}
