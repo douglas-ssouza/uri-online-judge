698da92c3ebaa171cb6fef273002ac04e4fbf29a
Douglas Soares de Souza <douglassouzabjj@hotmail.com>
2020-09-27 16:53:18 -0300

    SomaSimples completo

---
+++ Exercicios/SomaSimples.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            soma = a + b;
+
+            Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
