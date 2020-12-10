84b6e0db0f94604b752c27fe42cc6caf6893ced8
Douglas <douglassouzabjj@hotmail.com>
2020-12-10 19:45:13 -0300

    update 10/12

---
+++ Exercicios/AbaixoDaDiagonalPrincipal.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace AbaixoDaDiagonalPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            double[,] matriz;
+            matriz = new double[12, 12];
+
+            char operacao = Convert.ToChar(Console.ReadLine());
+            double soma = 0, media;
+            int quant = 0;
+
+            for (int i = 0; i < 12; i++){
+                for (int j = 0; j < 12; j++){
+                    matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+
+                    if (i > j){
+                        soma += matriz[i, j];
+                        quant++;
+                    }
+                }
+            }
+
+            if (operacao == 'S'){
+                Console.WriteLine("{0}", soma.ToString("0.0"));
+            }else{
+                media = soma / quant;
+                Console.WriteLine("{0}", media.ToString("0.0"));
+            }
+
+        }
+    }
+}
