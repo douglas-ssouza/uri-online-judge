01e370f291e6dbe8850892d817ed23473a862b3b
Douglas <douglassouzabjj@hotmail.com>
2020-12-11 00:12:24 -0300

    update11/12

---
+++ Exercicios/AreaEsquerda.cs
@@ -0,0 +1,37 @@
+using system;
+
+namespace AreaEsquerda
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            double[,] matriz;
+            matriz = new double[12, 12];
+
+            char operacao = Convert.ToChar(Console.ReadLine());
+            double soma = 0, media;
+            int quant = 0, limite = 11;
+
+            for (int i = 0; i < 12; i++){
+                for (int j = 0; j < 12; j++){
+                    matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+
+                    if (i > j && j < limite){
+                        soma += matriz[i, j];
+                        quant++;
+                    }
+                }
+                limite--;
+            }
+
+            if (operacao == 'S'){
+                Console.WriteLine("{0}", soma.ToString("0.0"));
+            }else{
+                media = soma / quant;
+                Console.WriteLine("{0}", media.ToString("0.0"));
+            }
+
+        }
+    }
+}
