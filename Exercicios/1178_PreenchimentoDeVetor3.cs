a474b3c5ddaf46a644c6c2fddb48312ec9ffb16b
Douglas <douglassouzabjj@hotmail.com>
2020-12-20 09:20:35 -0300

    Preenchimento de vetor 3 (incompleto)

--- Exercicios/1178_PreenchimentoDeVetor3.cs
+++ Exercicios/1178_PreenchimentoDeVetor3.cs
@@ -6,19 +6,7 @@ namespace PreenchimentoDeVetor3
     {
         static void Main(string[] args){
 
-        float[] n;
-        n = new float[100];
-
-        float x = float.Parse(Console.ReadLine());
-
-        for (int i = 0; i < 100; i++){
-            n[i] = x;
-            x /= 2;
-        }
-
-        for (int i = 0; i < 100; i++){
-            Console.WriteLine("N[" + i + "] = {0}", n[i].ToString("0.0000"));
-        }
+        
 
         }
     }
