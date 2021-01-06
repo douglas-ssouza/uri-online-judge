02e1d23da2914e68a673bdb4a7fff4b04576ac34
Douglas <douglassouzabjj@hotmail.com>
2021-01-06 15:06:40 -0300

    update 06/01/2021

--- Exercicios/iniciante/1957_ConverterParaHeadecimal.cs
+++ Exercicios/iniciante/1957_ConverterParaHeadecimal.cs
@@ -7,70 +7,10 @@ namespace ConverterParaHexadecimal
     {
         static void Main(string[] args){
 
-            int valor = Convert.ToInt32(Console.ReadLine());
-            string hexa = "";
+            int numero = Convert.ToInt32(Console.ReadLine());
+            string hexadecimal = numero.ToString("X");
 
-            if (valor > 0 && valor < 10){
-                hexa += Convert.ToString(valor);
-            }else if (valor < 16){
-                switch (valor){
-                    case 10:
-                        hexa += "A";
-                        break;
-                    case 11:
-                        hexa += "B";
-                        break;
-                    case 12:
-                        hexa += "C";
-                        break;
-                    case 13:
-                        hexa += "D";
-                        break;
-                    case 14:
-                        hexa += "E";
-                        break;
-                    case 15:
-                        hexa += "F";
-                        break;
-                }
-            }else{
-                while (valor >= 16){
-                    if ((valor % 16 >= 10) && (valor % 16 <= 15)){
-                        switch (valor){
-                            case 10:
-                                hexa += "A";
-                                break;
-                            case 11:
-                                hexa += "B";
-                                break;
-                            case 12:
-                                hexa += "C";
-                                break;
-                            case 13:
-                                hexa += "D";
-                                break;
-                            case 14:
-                                hexa += "E";
-                                break;
-                            case 15:
-                                hexa += "F";
-                                break;
-                        }
-                    }else if (valor % 16 > 0){
-                        hexa += Convert.ToString(valor % 16);
-                    }
-
-                    valor = valor / 16;
-
-                    if (valor < 16){
-                        hexa += Convert.ToString(valor / 16);
-                    }
-
-                }                   
-            }
-            
-
-            Console.WriteLine(hexa);
+            Console.WriteLine(hexadecimal);
 
         }
     }
