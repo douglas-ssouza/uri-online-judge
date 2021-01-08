9b58b32d809c5cf65124b723ffb980284d9d39b9
Douglas <douglassouzabjj@hotmail.com>
2021-01-08 09:39:42 -0300

    update 08/01/2021

---
+++ Exercicios/iniciante/1963_OFilme.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OFilme
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            string[] linha = Console.ReadLine().Split(' ');
+            double preco1 = Convert.ToDouble(linha[0]);
+            double preco2 = Convert.ToDouble(linha[1]);
+
+            
+
+        }
+    }
+}
