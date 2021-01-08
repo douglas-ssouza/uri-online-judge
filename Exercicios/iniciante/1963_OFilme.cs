f61cfae33d3ed9db6bc333ce065e7d5e2a04282d
Douglas <douglassouzabjj@hotmail.com>
2021-01-08 10:43:04 -0300

    update 08/01/2021

--- Exercicios/iniciante/1963_OFilme.cs
+++ Exercicios/iniciante/1963_OFilme.cs
@@ -10,7 +10,12 @@ namespace OFilme
             double preco1 = Convert.ToDouble(linha[0]);
             double preco2 = Convert.ToDouble(linha[1]);
 
-            
+            double dif, pct;
+
+            dif = preco2 - preco1;
+            pct = (dif * 100) / preco1;
+
+            Console.WriteLine("{0}%", pct.ToString("0.00"));
 
         }
     }
---
+++ Exercicios/iniciante/1973_JornadaNasEstrelas.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace JornadaNasEstrelas
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            int estrelasTotal, carneirosTotal = 0, visitas;
+            int[] estrelas;
+
+            estrelasTotal = Convert.ToInt32(Console.ReadLine());
+
+            estrelas = new int[estrelasTotal];
+            int louco = 0;
+            int maior = 0;
+            
+            string[] linha = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < estrelasTotal; i++){
+                estrelas[i] = Convert.ToInt32(linha[i]);
+                carneirosTotal += estrelas[i];
+            }
+
+            while (louco >= 0 && louco < estrelasTotal){
+
+                if (estrelas[louco] > 0){
+                    carneirosTotal--;
+                }
+
+                if (estrelas[louco] % 2 == 0){
+                    if (estrelas[louco] > 0){
+                        estrelas[louco]--;
+                    }
+                    louco--;
+                }else{
+                    estrelas[louco]--;
+                    louco++;
+                }
+
+                if (louco > maior){
+                    maior = louco;
+                }
+
+           }
+
+           Console.WriteLine(maior + 1 + " " + carneirosTotal);
+
+
+        }
+    }
+}
